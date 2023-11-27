42e76d0feb67ce6cae4d9dcc8c941d0967b0eb79
Amritpal Singh <100977877+AmritPal91@users.noreply.github.com>
2023-11-27 23:38:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let typed = new Typed('#myName', {
-    strings: ['I am Amritpal Singh','Based in India, 'I am a front-end developer passionate about building accessible web apps that users love.'],
+    strings: ['I am Amritpal Singh','Based in India,', 'I am a front-end developer passionate about building accessible web apps that users love.'],
     typeSpeed: 100,
     showCursor: false
 })
